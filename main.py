05e5c55f56a59a5b9d813fe59ebd9c087ec40fdc
Ruizhi Liao <ruizhi@csail.mit.edu>
2020-12-14 16:57:39 -0500

    adding config and executor

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from pynetdicom import VerificationPresentationContexts
 from mlserver import transfer_syntax
 from mlserver.core import DicomSaver
 from mlserver.database import Database
+from mlserver.executor import DelayedExecutor
 from mlserver.utils import logged_method
 from mlserver.utils import try_except
 
@@ -40,6 +41,58 @@ class ApplicationEntity(_ApplicationEntity):
 
     def start_server(self, *args, **kwargs):
         logging.info('Server started.')
+        print('Server started.')
         super(ApplicationEntity, self).start_server(
             (self._host, self._port),
             *args, **kwargs)
+
+
+class Helper(object):
+    def __init__(self):
+        """Shared resources across all threads."""
+        # self._model = Model() #TODO: replace this with our cxr model
+        self._executor = DelayedExecutor()
+        self._executor.start()
+
+    @property
+    def handlers(self):
+        return [
+            (evt.EVT_C_STORE, self.handle_c_store)]
+
+    @try_except(error_code=0xA700)
+    @logged_method
+    def handle_c_store(self, event):
+        ds = event.dataset
+        ds.file_meta = event.file_meta
+        ds.is_little_endian = ds.file_meta.TransferSyntaxUID.is_little_endian
+        ds.is_implicit_VR = ds.file_meta.TransferSyntaxUID.is_implicit_VR
+
+        DicomSaver()(ds)
+        Database().add(cls=Database.Patient, ds=ds).add(cls=Database.Study, ds=ds)
+
+        study_name = ds.AccessionNumber
+        self._executor.delayed_run(
+            key=study_name,
+            fn=functools.partial(self._process_study, study_name=study_name))
+
+    @try_except(error_code=0xC2FF)
+    @logged_method
+    def _process_study(self, study_name):
+        NiftiConverter()(study_name)
+        self._model(study_name)
+        JsonConverter()(study_name, organs=self._model.organs)
+        SlicePlotter()(study_name)
+        Database().add(Database.SegmentationVol, study_name=study_name)
+
+
+def main(_):
+    gin.parse_config_file(FLAGS.gin_file)
+    # warnings.filterwarnings('ignore') #TODO: revisit if this is necessary
+    logging.get_absl_handler().use_absl_log_file('mlserver')
+
+    ae = ApplicationEntity()
+    ae.start_server(evt_handlers=Helper().handlers)
+
+
+if __name__ == '__main__':
+    app.run(main)
